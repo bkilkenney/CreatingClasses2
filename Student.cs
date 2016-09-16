500c1a54ad8f35033dbf442441e33bbfb0e3840b
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-16 10:44:50 -0400

    New Methods Added

--- Student.cs
+++ Student.cs
@@ -8,5 +8,57 @@ namespace CreatingClasses2
 {
     class Student
     {
+        //These are fields
+        private string fullName;
+        private int age;
+        private string university;
+        private string major;
+        private float gpa;
+        
+        //Properties
+        public string name
+        {
+            get { return this.fullName; }
+            set { this.fullName = value; }
+        }
+        
+       public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+
+        public string University
+        {
+            get { return this.university; }
+        }
+
+        public string Major
+        {
+            get;
+            set;
+        }
+
+        public float GPA 
+            {
+                get { return this.gpa; }
+                set { this.gpa = value; }
+            }
+        
+        //Constructors
+         public Student()
+        {
+           
+        }
+
+        public Student(string fullName, int age, string university, string major, float gpa)
+            
+        {
+            this.fullName = fullName;
+            this.age = age;
+            this.university = university;
+            this.major = major;
+            this.gpa = gpa;
+        }
     }
 }
